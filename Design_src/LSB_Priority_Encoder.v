1b64ad7f9d37256a719565d1440c7e6fba06beaa
amirshmul <86232400+amirshmul@users.noreply.github.com>
2021-06-21 13:47:13 +0300

    Update LSB_Priority_Encoder.v

--- Design_src/LSB_Priority_Encoder.v
+++ Design_src/LSB_Priority_Encoder.v
@@ -9,7 +9,7 @@ module LSB_Priority_Encoder(
     always @(In_Bus) begin: Priority_Comb
         casez(In_Bus)
             4'b???1: begin
-                valid <= 1'b1;
+                valid <= 1'b1; // Use blocking assignment
                 Priority <= 2'b00;
                 end
                 
@@ -34,5 +34,20 @@ module LSB_Priority_Encoder(
                 end
         endcase
     end
+    
+//    Generally speaking, 
+//1. It is best to avoid the use of casex/casez if not necessary.
+//2. On the other hand, try to use SystemVerilog Priority/Unique cases when possible.
+//3. Never ever use Verilog obsolete "full_case" "parallel_case".
+
+//casex/casez:
+//http://www.verilogpro.com/verilog-case-casez-casex/ 
+//unique/priority:
+//https://www.verilogpro.com/systemverilog-unique-priority/  
+//"full_case parallel_case", the Evil Twins of Verilog Synthesis:
+//http://www.sunburst-design.com/papers/CummingsSNUG1999Boston_FullParallelCase_rev1_1.pdf 
+
+
+    // How would you write a generic design here? (input width "W_DATA")?
     
 endmodule
